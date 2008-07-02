9210a5b317bf3a8d59875362a3e4ad5cd67917d4
andreas.pelme <andreas.pelme@98d35234-f74b-0410-9e22-51d878bdf110>
2008-07-02 05:31:12 +0000

    Fixed bad import in csstidy_python filter. Thanks Clint Ecker!
    git-svn-id: https://django-compress.googlecode.com/svn/trunk@68 98d35234-f74b-0410-9e22-51d878bdf110

--- compress/filters/csstidy_python/__init__.py
+++ compress/filters/csstidy_python/__init__.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 
 from compress.filter_base import FilterBase
-from compress.filters.csstidy.csstidy import CSSTidy
+from compress.filters.csstidy_python.csstidy import CSSTidy
 
 COMPRESS_CSSTIDY_SETTINGS = getattr(settings, 'COMPRESS_CSSTIDY_SETTINGS', {})
 
