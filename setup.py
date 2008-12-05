7df33871f3e1b3e22d85143b81d6d165290e0971
Andreas Pelme <andreas@pelme.se>
2008-12-05 12:52:45 +0100

    Set version to 1.0.1 for upload to PyPI.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ for dirpath, dirnames, filenames in os.walk('compress'):
 
 setup(
     name='django-compress',
-    version='1.0',
+    version='1.0.1',
     description='django-compress provides an automated system for compressing CSS and JavaScript files',
     author='Andreas Pelme',
     author_email='Andreas Pelme <andreas@pelme.se>',
