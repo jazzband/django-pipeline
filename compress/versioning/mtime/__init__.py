03fd9095329d13b397847ab1def28376d70ae360
Andreas Pelme <andreas@pelme.se>
2008-12-05 11:43:09 +0100

    Fixed bug in mtime versioning that caused re-generation of files every time. Make sure to treat mtimes as ints to avoid comparision errors. Thanks jbmendelson for the bug report.

--- compress/versioning/mtime/__init__.py
+++ compress/versioning/mtime/__init__.py
@@ -3,23 +3,17 @@ import os
 from compress.utils import get_output_filename, media_root
 from compress.versioning.base import VersioningBase
 
-def max_mtime(files):
-    return int(max([os.stat(media_root(f)).st_mtime for f in files]))
-
 class MTimeVersioning(VersioningBase):
-    
+
     def get_version(self, source_files):
-        mtime = max_mtime(source_files)
-        try:
-            return str(int(mtime))
-        except ValueError:
-            return str(mtime)
-        
+
+        # Return the modification time for the newest source file
+        return str(max([int(os.stat(media_root(f)).st_mtime) for f in source_files]))
+
     def needs_update(self, output_file, source_files, version):
 
         output_file_name = get_output_filename(output_file, version)
         compressed_file_full = media_root(output_file_name)
 
-        return (os.stat(compressed_file_full).st_mtime < version), version
-            
-        
+        return (int(os.stat(compressed_file_full).st_mtime) < int(version)), version
+
