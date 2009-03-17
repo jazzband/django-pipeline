0ee308a2471879a46b5f62503ea9fc15558794f4
Peter Baumgartner <pete@lincolnloop.com>
2009-03-17 13:58:34 +0800

    allows for source file root, compressed file root and compressed file URL to vary from MEDIA_ROOT and MEDIA_URL while retaining backwards compatibility. Optional settings are COMPRESS_SOURCE, COMPRESS_ROOT, COMPRESS_URL. Needs docs.
    Signed-off-by: Patrick Altman <patrick@studionow.com>

--- compress/versioning/mtime/__init__.py
+++ compress/versioning/mtime/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from compress.utils import get_output_filename, media_root
+from compress.utils import get_output_filename, compress_source, compress_root
 from compress.versioning.base import VersioningBase
 
 class MTimeVersioning(VersioningBase):
@@ -8,12 +8,13 @@ class MTimeVersioning(VersioningBase):
     def get_version(self, source_files):
 
         # Return the modification time for the newest source file
-        return str(max([int(os.stat(media_root(f)).st_mtime) for f in source_files]))
+        return str(max(
+                [int(os.stat(compress_source(f)).st_mtime) for f in source_files]))
 
     def needs_update(self, output_file, source_files, version):
 
         output_file_name = get_output_filename(output_file, version)
-        compressed_file_full = media_root(output_file_name)
+        compressed_file_full = compress_root(output_file_name)
 
         return (int(os.stat(compressed_file_full).st_mtime) < int(version)), version
 
