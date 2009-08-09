8d915c5c55c24665b8002ba40190d1aa2e181c72
Bryan Chow <bryan@fullfactor.com>
2009-08-09 00:22:20 -0700

    Modify get_version_from_file return newest version instead of the first match. This is horribly inefficient and is a temporary solution.

--- compress/utils.py
+++ compress/utils.py
@@ -103,10 +103,13 @@ def get_version(source_files, verbosity=0):
     
 def get_version_from_file(path, filename):
     regex = re.compile(r'^%s$' % (get_output_filename(settings.COMPRESS_VERSION_PLACEHOLDER.join([re.escape(part) for part in filename.split(settings.COMPRESS_VERSION_PLACEHOLDER)]), r'([A-Za-z0-9]+)')))
+    results = []
     for f in os.listdir(path):
         result = regex.match(f)
         if result and result.groups():
-            return result.groups()[0]
+            results.append(result.groups()[0])
+    results.sort()
+    return results[-1]
 
 def remove_files(path, filename, verbosity=0):    
     regex = re.compile(r'^%s$' % (os.path.basename(get_output_filename(settings.COMPRESS_VERSION_PLACEHOLDER.join([re.escape(part) for part in filename.split(settings.COMPRESS_VERSION_PLACEHOLDER)]), r'[A-Za-z0-9]+'))))
