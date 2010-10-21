5f0d598b827d1dc518193143e62a866e172d6a0f
ara818 <ara818@gmail.com>
2010-10-21 18:49:36 -0400

    Add versioning based on Git revisions. This provides a fast way to form Javascript versions that will be consistent when multiservers try to form versioned files.

--- compress/utils.py
+++ compress/utils.py
@@ -37,6 +37,17 @@ def get_mod_func(callback):
     except ValueError:
         return callback, ''
     return callback[:dot], callback[dot+1:]
+    
+def get_hexdigest(plaintext):
+    """
+    Create a hexdigest from a plaintext string
+    """
+    try:
+        import hashlib
+        return hashlib.sha1(plaintext).hexdigest()
+    except ImportError:
+        import sha
+        return sha.new(plaintext).hexdigest()
 
 def needs_update(output_file, source_files, verbosity=0):
     """
