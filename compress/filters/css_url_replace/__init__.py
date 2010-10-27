246b10937a43df6d23a372ff865c35822657bce2
Timothée Peignier <timothee.peignier@tryphon.org>
2010-10-27 10:18:19 +0200

    make css url replace more versatile

--- compress/filters/css_url_replace/__init__.py
+++ compress/filters/css_url_replace/__init__.py
@@ -1,17 +1,14 @@
 from django.conf import settings
 
-from compress.filter_base import FilterBase, FilterError
+from compress.filter_base import FilterBase
 
-REPLACE_LIST = getattr(settings, 'COMPRESS_CSS_URL_REPLACE', [])
+CSS_REPLACE = getattr(settings, 'COMPRESS_CSS_URL_REPLACE', [])
 
 class CSSURLReplace(FilterBase):
 
     def filter_css(self, css):
-        filtered_css = css
-        if type(REPLACE_LIST) == list:
-            for REPLACE in REPLACE_LIST:
-                if len(REPLACE) == 2:
-                    filtered_css = filtered_css.replace(REPLACE[0], REPLACE[1])
-                    if self.verbose:
-                        print 'Replaced "%s" with "%s"' % REPLACE
-        return filtered_css
+        for old, new in CSS_REPLACE.iteritems():
+            css = css.replace(old, new)
+            if self.verbose:
+                print 'Replaced "%s" with "%s"' % (old, new)
+        return css
