24a719346bf0a869e970b213cecf2e6de623c624
Timothée Peignier <timothee.peignier@tryphon.org>
2011-01-28 13:04:20 +0100

    make css replace use regexp

--- compress/filters/css_url_replace/__init__.py
+++ compress/filters/css_url_replace/__init__.py
@@ -1,3 +1,4 @@
+import re
 from django.conf import settings
 
 from compress.filter_base import FilterBase
@@ -5,10 +6,9 @@ from compress.filter_base import FilterBase
 CSS_REPLACE = getattr(settings, 'COMPRESS_CSS_URL_REPLACE', [])
 
 class CSSURLReplace(FilterBase):
-
     def filter_css(self, css):
-        for old, new in CSS_REPLACE.iteritems():
-            css = css.replace(old, new)
+        for pattern, repl in CSS_REPLACE.iteritems():
+            css = re.sub(pattern, repl, css, flags=re.UNICODE | re.IGNORECASE)
             if self.verbose:
-                print 'Replaced "%s" with "%s"' % (old, new)
+                print 'Replaced "%s" with "%s"' % (pattern, repl)
         return css
