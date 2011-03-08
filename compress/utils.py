770b8755ba82b20b8608594f8580c0042e333863
Sam Thomson <sammthomson@gmail.com>
2011-03-08 12:59:20 -0500

    sort to get the most recent compressed file rely on the fact that compressed filenames always increase. os.listdir returns filenames in arbitrary order, so they must be sorted (in reverse order) to get the most recent

--- compress/utils.py
+++ compress/utils.py
@@ -100,7 +100,7 @@ def get_version(source_files, verbosity=0):
     
 def get_version_from_file(path, filename):
     regex = re.compile(r'^%s$' % (get_output_filename(settings.COMPRESS_VERSION_PLACEHOLDER.join([re.escape(part) for part in filename.split(settings.COMPRESS_VERSION_PLACEHOLDER)]), r'([A-Za-z0-9]+)')))
-    for f in os.listdir(path):
+    for f in sorted(os.listdir(path), reverse=True):
         result = regex.match(f)
         if result and result.groups():
             return result.groups()[0]
@@ -108,7 +108,7 @@ def get_version_from_file(path, filename):
 def remove_files(path, filename, verbosity=0):    
     regex = re.compile(r'^%s$' % (os.path.basename(get_output_filename(settings.COMPRESS_VERSION_PLACEHOLDER.join([re.escape(part) for part in filename.split(settings.COMPRESS_VERSION_PLACEHOLDER)]), r'[A-Za-z0-9]+'))))
     if os.path.exists(path):
-        for f in sorted(os.listdir(path), reverse=True):
+        for f in os.listdir(path):
             if regex.match(f):
                 if verbosity >= 1:
                     print "Removing outdated file %s" % f
