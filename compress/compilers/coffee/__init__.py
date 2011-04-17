312f5a71025e21a33149ee0c2f188de32bbab1f6
Timothée Peignier <timothee.peignier@tryphon.org>
2011-04-17 15:43:32 +0200

    fix coffee settings

--- compress/compilers/coffee/__init__.py
+++ compress/compilers/coffee/__init__.py
@@ -7,7 +7,7 @@ class CoffeeScriptCompiler(SubProcessCompiler):
         return filename.endswith('.coffee')
 
     def compile_file(self, content):
-        command = "%s %s" % (settings.COFFEE_SCRIPT_BINARY, settings.COFFEE_SCRIPT_ARGUMENTS)
+        command = "%s %s" % (settings.COMPRESS_COFFEE_SCRIPT_BINARY, settings.COMPRESS_COFFEE_SCRIPT_ARGUMENTS)
         if self.verbose:
             command += '--verbose'
         return self.execute_command(command, content)
