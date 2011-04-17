bca184da24a460e769855b7a95dea86ae6aec0dc
Timothée Peignier <timothee.peignier@tryphon.org>
2011-04-17 15:43:51 +0200

    add sass compiler

---
+++ compress/compilers/sass/__init__.py
@@ -0,0 +1,13 @@
+from compress.conf import settings
+from compress.compilers import SubProcessCompiler
+
+
+class SASSCompiler(SubProcessCompiler):
+    def match_file(self, filename):
+        return filename.endswith('.scss')
+
+    def compile_file(self, content):
+        command = "%s %s" % (settings.COMPRESS_SASS_BINARY, settings.COMPRESS_SASS_ARGUMENTS)
+        if self.verbose:
+            command += '--verbose'
+        return self.execute_command(command, content)
