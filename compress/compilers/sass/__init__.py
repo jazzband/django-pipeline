67b3e9d0aa82d2250748056df7ff495fac93ef64
Timothée Peignier <timothee.peignier@tryphon.org>
2011-04-25 21:38:42 +0200

    make required options obligatory

--- compress/compilers/sass/__init__.py
+++ compress/compilers/sass/__init__.py
@@ -9,7 +9,7 @@ class SASSCompiler(SubProcessCompiler):
         return filename.endswith('.scss')
 
     def compile_file(self, content):
-        command = "%s %s" % (settings.COMPRESS_SASS_BINARY, settings.COMPRESS_SASS_ARGUMENTS)
+        command = "%s --scss %s" % (settings.COMPRESS_SASS_BINARY, settings.COMPRESS_SASS_ARGUMENTS)
         if self.verbose:
             command += '--verbose'
         return self.execute_command(command, content)
