c5e598a841b95099308077312ee340297ce6f06c
Timothée Peignier <timothee.peignier@tryphon.org>
2011-11-27 20:27:40 +0100

    improve test runner

---
+++ tests/settings.py
@@ -0,0 +1,31 @@
+import os
+import os
+local_path = lambda path: os.path.join(os.path.dirname(__file__), path)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'TEST_NAME': ':memory:'
+    }
+}
+
+INSTALLED_APPS = [
+    'django.contrib.contenttypes',
+    'django.contrib.sites',
+    'django.contrib.auth',
+    'django.contrib.admin',
+    'pipeline',
+    'tests',
+]
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = local_path('media')
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = local_path('assets')
+
+TEMPLATE_DIRS = (
+    local_path('templates'),
+)
