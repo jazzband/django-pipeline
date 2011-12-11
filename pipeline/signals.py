df2f9c8d41ca76a16f015f15f7c0611598a712f1
Timothée Peignier <timothee.peignier@tryphon.org>
2011-12-11 20:27:18 +0100

    remove version from signals args

--- pipeline/signals.py
+++ pipeline/signals.py
@@ -1,5 +1,5 @@
 from django.dispatch import Signal
 
 
-css_compressed = Signal(providing_args=["package", "version"])
-js_compressed = Signal(providing_args=["package", "version"])
+css_compressed = Signal(providing_args=["package"])
+js_compressed = Signal(providing_args=["package"])
