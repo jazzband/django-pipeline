efefc1194836e251815983b8340b65eaaa3bbcdd
Steven Cummings <estebistec@gmail.com>
2012-03-09 23:32:08 -0600

    cssmin compressor including doc; and a correction to the sample custom compressor

---
+++ pipeline/compressors/cssmin/__init__.py
@@ -0,0 +1,13 @@
+from pipeline.compressors import CompressorBase
+
+
+class CssminCompressor(CompressorBase):
+    """
+    CSS compressor based on the Python library cssmin
+    (http://pypi.python.org/pypi/cssmin/).
+
+    """
+
+    def compress_css(self, css):
+        from cssmin import cssmin
+        return cssmin(css)
