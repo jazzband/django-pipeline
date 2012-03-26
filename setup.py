ce603401624782573e6cf51a5b4a5f780a80201d
Timothée Peignier <timothee.peignier@tryphon.org>
2012-03-26 20:22:47 +0200

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-pipeline',
-    version='1.2c2',
+    version='1.2.0',
     description='Pipeline is an asset packaging library for Django.',
     long_description="""Pipeline is an asset packaging library for Django, providing
 both CSS and JavaScript concatenation and compression, built-in JavaScript
