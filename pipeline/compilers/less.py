3fc838e584e51ce35a7dd061217fd94176adf729
Rémy Sanchez <remy.sanchez@hyperthese.net>
2012-12-03 21:43:12 +0100

    Implementing get_search_path() in LESS compiler

--- pipeline/compilers/less.py
+++ pipeline/compilers/less.py
@@ -13,6 +13,28 @@ class LessFileTree(BaseFileTree):
 
 class LessCompiler(CssCompiler):
     tree_object = LessFileTree
+    search_path = None
+
+    @staticmethod
+    def get_search_path():
+        if LessCompiler.search_path is not None:
+            return LessCompiler.search_path
+
+        argv = shlex.split(settings.PIPELINE_LESS_ARGUMENTS)
+
+        path_str = "."
+        for i in range(1, len(argv)):
+            if argv[i - 1] == "--include-path":
+                path_str = argv[i]
+                break
+
+        if os.name != 'nt':
+            sep = ':'
+        else:
+            sep = ';'
+
+        LessCompiler.search_path = path_str.split(sep)
+        return LessCompiler.search_path
 
     def compile_file(self, infile, outfile, outdated=False, force=False):
         if not (outdated or force):
