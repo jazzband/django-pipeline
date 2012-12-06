cd0568fbcffd0fe405e4c04a02e0dd84c532a171
Steven Cummings <estebistec@gmail.com>
2012-12-05 21:26:24 -0600

    Provide attempted command on IOError from pipe

--- pipeline/compressors/__init__.py
+++ pipeline/compressors/__init__.py
@@ -232,7 +232,11 @@ class SubProcessCompressor(CompressorBase):
     def execute_command(self, command, content):
         pipe = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE,
             stdin=subprocess.PIPE, stderr=subprocess.PIPE)
-        pipe.stdin.write(smart_str(content))
+        try:
+            pipe.stdin.write(smart_str(content))
+        except IOError:
+            error = "Unable to pipe content to command: %s" % command
+            raise CompressorError(error)
         pipe.stdin.close()
 
         compressed_content = pipe.stdout.read()
