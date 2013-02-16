743a7248b3da84de8d78570deefa24833171c192
Timothée Peignier <timothee.peignier@tryphon.org>
2013-02-16 10:14:04 +0100

    remove uglifyjs default argument

--- pipeline/compressors/uglifyjs.py
+++ pipeline/compressors/uglifyjs.py
@@ -6,7 +6,7 @@ from pipeline.compressors import SubProcessCompressor
 
 class UglifyJSCompressor(SubProcessCompressor):
     def compress_js(self, js):
-        command = '%s -nc %s' % (settings.PIPELINE_UGLIFYJS_BINARY, settings.PIPELINE_UGLIFYJS_ARGUMENTS)
+        command = '%s %s' % (settings.PIPELINE_UGLIFYJS_BINARY, settings.PIPELINE_UGLIFYJS_ARGUMENTS)
         if self.verbose:
             command += ' --verbose'
         return self.execute_command(command, js)
