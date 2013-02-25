e33375455174adb37e568efa2eccb988a86e132b
Venelin Stoykov <venelin@magicsolutions.bg>
2013-02-25 10:12:10 +0200

    On debug does not minify html
    If settings.DEBUG is True developer does not want minifyed html. This
    is needed only in production enviroment.

--- pipeline/middleware.py
+++ pipeline/middleware.py
@@ -1,10 +1,18 @@
 from __future__ import unicode_literals
 
+from django.core.exceptions import MiddlewareNotUsed
 from django.utils.encoding import DjangoUnicodeDecodeError
 from django.utils.html import strip_spaces_between_tags as minify_html
 
+from pipeline.conf import settings
+
 
 class MinifyHTMLMiddleware(object):
+    def __init__(self):
+        if settings.DEBUG:
+            # On debug does not minify html
+            raise MiddlewareNotUsed
+
     def process_response(self, request, response):
         if response.has_header('Content-Type') and 'text/html' in response['Content-Type']:
             try:
