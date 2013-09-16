4b7be14f7a2eff3056b076b5c68d433bc84dad66
Miroslav Shubernetskiy <miroslav@miki725.com>
2013-09-16 10:53:08 -0400

    fix for less compiler for non-relative url imports (#258)

--- pipeline/compilers/less.py
+++ pipeline/compilers/less.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from os.path import dirname
+from django.contrib.staticfiles.finders import get_finders
 
 from pipeline.conf import settings
 from pipeline.compilers import SubProcessCompiler
@@ -13,10 +14,39 @@ class LessCompiler(SubProcessCompiler):
         return filename.endswith('.less')
 
     def compile_file(self, infile, outfile, outdated=False, force=False):
-        command = "%s %s %s %s" % (
+        command = '%s %s %s %s --include-path="%s"' % (
             settings.PIPELINE_LESS_BINARY,
             settings.PIPELINE_LESS_ARGUMENTS,
             infile,
-            outfile
+            outfile,
+            ':'.join(self.get_all_static_dirs())
         )
         return self.execute_command(command, cwd=dirname(infile))
+
+    def get_all_static_dirs(self):
+        """
+        Get a list of all static file dirs which can be searched as
+        defined in ``django.conf.settings.STATICFILES_FINDERS``.
+
+        This is helpful for a less compiler if there are imports within
+        less files which reference other static files in different
+        Django apps. By providing the compiler a list of folders it
+        should consider, it allows it to resolve the import paths.
+        """
+        dirs = set()
+
+        for finder in get_finders():
+            if hasattr(finder, 'storage'):
+                storages = [finder.storage]
+            elif hasattr(finder, 'storages'):
+                storages = finder.storages.values()
+            else:
+                continue
+
+            for storage in storages:
+                if not hasattr(storage, 'location'):
+                    continue
+
+                dirs.add(dirname(storage.location))
+
+        return list(dirs)
