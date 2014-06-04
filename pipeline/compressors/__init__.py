c832dce8c426b03cb4daa6cd79316961b3340857
Alex Ehlke <alex.ehlke@tophatmonocle.com>
2014-06-04 16:45:11 -0400

    add about: to list of non-rewritable URLs

--- pipeline/compressors/__init__.py
+++ pipeline/compressors/__init__.py
@@ -16,7 +16,7 @@ from pipeline.exceptions import CompressorError
 
 URL_DETECTOR = r'url\([\'"]?([^\s)]+\.[a-z]+[^\'"\s]*)[\'"]?\)'
 URL_REPLACER = r'url\(__EMBED__(.+?)(\?\d+)?\)'
-NON_REWRITABLE_URL = re.compile(r'^(http:|https:|data:|//)')
+NON_REWRITABLE_URL = re.compile(r'^(http:|https:|data:|about:|//)')
 
 DEFAULT_TEMPLATE_FUNC = "template"
 TEMPLATE_FUNC = r"""var template = function(str){var fn = new Function('obj', 'var __p=[],print=function(){__p.push.apply(__p,arguments);};with(obj||{}){__p.push(\''+str.replace(/\\/g, '\\\\').replace(/'/g, "\\'").replace(/<%=([\s\S]+?)%>/g,function(match,code){return "',"+code.replace(/\\'/g, "'")+",'";}).replace(/<%([\s\S]+?)%>/g,function(match,code){return "');"+code.replace(/\\'/g, "'").replace(/[\r\n\t]/g,' ')+"__p.push('";}).replace(/\r/g,'\\r').replace(/\n/g,'\\n').replace(/\t/g,'\\t')+"');}return __p.join('');");return fn;};"""
