556dfc46bef9e389555bf1907cc60c486f70d48e
Timothée Peignier <timothee.peignier@tryphon.org>
2014-07-26 11:27:23 -0700

    fix django 1.7 tests warning

--- tests/settings.py
+++ tests/settings.py
@@ -30,6 +30,11 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'tests.urls'
 
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware'
+)
+
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = local_path('media')
