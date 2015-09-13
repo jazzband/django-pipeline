8be17931fcb6ae530d41be5fd1ea2e847be7e79b
Chad Miller <chad.miller@canonical.com>
2015-09-13 10:11:30 -0400

    Warn developer when a template-tag package is bad
    Fixes #497.

--- pipeline/templatetags/pipeline.py
+++ pipeline/templatetags/pipeline.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
 
+import logging
+
 from django.contrib.staticfiles.storage import staticfiles_storage
 
 from django import template
@@ -73,6 +75,7 @@ class StylesheetNode(PipelineMixin, template.Node):
         try:
             package = self.package_for(package_name, 'css')
         except PackageNotFound:
+            logging.warn("Package %r is unknown. Check PIPELINE_CSS in your settings.", package_name)
             return ''  # fail silently, do not return anything if an invalid group is specified
         return self.render_compressed(package, 'css')
 
@@ -101,6 +104,7 @@ class JavascriptNode(PipelineMixin, template.Node):
         try:
             package = self.package_for(package_name, 'js')
         except PackageNotFound:
+            logging.warn("Package %r is unknown. Check PIPELINE_JS in your settings.", package_name)
             return ''  # fail silently, do not return anything if an invalid group is specified
         return self.render_compressed(package, 'js')
 
