386f63ac610d54f803335978c6acc89b1ce9e014
Danielle Knudson <knudson.danielle@gmail.com>
2019-11-21 10:52:10 -0800

    Override bug in find() method

--- pipeline/finders.py
+++ pipeline/finders.py
@@ -13,10 +13,7 @@ class PipelineFinder(BaseStorageFinder):
     storage = staticfiles_storage
 
     def find(self, path, all=False):
-        if not settings.PIPELINE_ENABLED:
-            return super(PipelineFinder, self).find(path, all)
-        else:
-            return []
+        return super(PipelineFinder, self).find(path, all)
 
     def list(self, ignore_patterns):
         return []
