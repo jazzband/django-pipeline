4e3bb089b687a837200afb22d84123f938237ecc
Gagan Deep <pandafy.dev@gmail.com>
2021-05-22 01:22:28 +0530

    [change] Added minify_html package

--- pipeline/middleware.py
+++ pipeline/middleware.py
@@ -1,6 +1,6 @@
 from django.core.exceptions import MiddlewareNotUsed
 from django.utils.encoding import DjangoUnicodeDecodeError
-from django.utils.html import strip_spaces_between_tags as minify_html
+import minify_html
 
 from pipeline.conf import settings
 
@@ -16,7 +16,7 @@ class MinifyHTMLMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
         if response.has_header('Content-Type') and 'text/html' in response['Content-Type']:
             try:
-                response.content = minify_html(response.content.decode('utf-8').strip())
+                response.content = minify_html.minify(response.content.decode('utf-8').strip())
                 response['Content-Length'] = str(len(response.content))
             except DjangoUnicodeDecodeError:
                 pass
