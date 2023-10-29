30107f5b5d4a01b573f76cca8c0af302638735db
d9pouces <github@19pouces.net>
2023-10-29 11:12:41 +0100

    feat: avoid the use of pkg_resources when importlib.metadata, since pkg_resources is removed from Python 3.12

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -3,16 +3,18 @@ try:
     from importlib.metadata import PackageNotFoundError, version as get_version
 except ImportError:
     get_version = None
-try:
-    from pkg_resources import DistributionNotFound as PackageNotFoundError, get_distribution
+
+if get_version is None:
+    try:
+        from pkg_resources import DistributionNotFound as PackageNotFoundError, get_distribution
 
 
-    def get_version(x):
-        return get_distribution(x).version
-except ImportError:
-    get_version = None
-    PackageNotFoundError = None
-    get_distribution = None
+        def get_version(x):
+            return get_distribution(x).version
+    except ImportError:
+        get_version = None
+        PackageNotFoundError = None
+        get_distribution = None
 
 __version__ = None
 if get_version is not None:
