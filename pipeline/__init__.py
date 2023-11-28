d3a7144f9a1cf2deca02b95197571dad1b920834
d9pouces <github@19pouces.net>
2023-11-28 22:15:59 +0100

    fix: remove flake8 errors

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -1,24 +1,27 @@
 PackageNotFoundError = None
+DistributionNotFound = None
 try:
     from importlib.metadata import PackageNotFoundError
     from importlib.metadata import version as get_version
 except ImportError:
     get_version = None
-try:
-    from pkg_resources import DistributionNotFound as PackageNotFoundError
-    from pkg_resources import get_distribution
-
-
-    def get_version(x):
-        return get_distribution(x).version
-except ImportError:
-    get_version = None
     PackageNotFoundError = None
-    get_distribution = None
+if get_version is None:
+    try:
+        from pkg_resources import DistributionNotFound, get_distribution
+
+        def get_version(x):
+            return get_distribution(x).version
+    except ImportError:
+        get_version = None
+        DistributionNotFound = None
+        get_distribution = None
 
 __version__ = None
 if get_version is not None:
     try:
         __version__ = get_version("django-pipeline")
     except PackageNotFoundError:
+        pass
+    except DistributionNotFound:
         pass
